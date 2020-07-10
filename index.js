ea2c890bee8be2c5da12494da2e062a8b3a505ed
Revlink <latinreve@gmailcom>
2020-07-10 04:31:15 -0400

    Fixed package.json file

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-console */
-const db = require('./jsonManager')('./data.json');
+// ! EMPTY ARGUMENT ERROR HANDLING
+const db = require("./jsonManager")("./data.json");
+// ! EMPTY ARGUMENT ERROR HANDLING
 
 db.Start()
-    .then(async (docs) => {
-        const a = db.Find('maquinas', {
-            property: 'description',
-            toSearch: 'vestibulum',
-        });
-
-       db.Filter('users',{})
-
-
-
-        db.Save();
-    })
-    .finally(() => {
-        db.Close();
+  .then(async (docs) => {
+    const a = db.Find("maquinas", {
+      property: "description",
+      toSearch: "vestibulum",
     });
+
+    let b = db.Filter("users", { property: "name", filterString: "Joel" });
+    console.log(b);
+
+    db.Save();
+  })
+  .finally(() => {
+    db.Close();
+  });
